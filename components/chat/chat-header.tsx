c0396a4ab4581494c01cf59e557681ca652669ce
Rishikesh Suvarna <rishikeshsuvarna@gmail.com>
2023-10-01 13:39:17 +0100

    Completed Video and Audio Channels setup with LiveKit

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import MobileToggle from '@/components/mobile-toggle'
 import { UserAvatar } from '@/components/user-avatar'
 import { SocketIndicator } from '../socket-indicator'
+import { ChatVideoButton } from './chat-video-button'
 
 interface ChatHeaderProps {
     serverId: string,
@@ -37,6 +38,7 @@ const ChatHeader = ({
         }
         <p className='font-semibold text-md text-black dark:text-white'>{name}</p>
         <div className="ml-auto flex items-center">
+            {type === 'conversation' && <ChatVideoButton />}
             <SocketIndicator />
         </div>
     </div>
